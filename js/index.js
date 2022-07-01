a3bf6982dea0ec49e1bf6fc7f4cc11815a427bc0
Raphael-Goulart <raphael.ngoulart@gmail.com>
2022-07-01 17:47:36 +0100

    BMI - calculate

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
-var titulo = document.querySelector('.titulo');
-
+/*var titulo = document.querySelector('.titulo');
 console.log(titulo.textContent);
+titulo.textContent = "Cálculo IMC"*/
 
-titulo.textContent = "Cálculo IMC"
+var paciente = document.querySelector('#paciente--um');
+
+var tdPeso = paciente.querySelector('.peso--um');
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector('.altura--um')
+var altura = tdAltura.textContent;
+
+var tdImc = paciente.querySelector('.info--imc');
+
+var imc = peso / (altura * altura); 
+
+tdImc.textContent = imc 
+
