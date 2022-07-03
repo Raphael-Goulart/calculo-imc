6939655628ba3612ffc6ef4c6a01f3e6eb3fb5c0
Raphael-Goulart <raphael.ngoulart@gmail.com>
2022-07-03 18:07:32 +0100

    created style css

--- js/index.js
+++ js/index.js
@@ -1,18 +1,43 @@
-/*var titulo = document.querySelector('.titulo');
-console.log(titulo.textContent);
-titulo.textContent = "Cálculo IMC"*/
+var titulo = document.querySelector('.titulo');
+titulo.textContent = "Cálculo IMC"
 
-var paciente = document.querySelector('#paciente--um');
+var pacientes = document.querySelectorAll('.paciente'); //traz todos os pacientes com a classe paciente All
 
-var tdPeso = paciente.querySelector('.peso--um');
-var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector('.altura--um')
-var altura = tdAltura.textContent;
 
-var tdImc = paciente.querySelector('.info--imc');
+for(var i = 0; i < pacientes.length; i++){
+    var cliente = pacientes[i];
 
-var imc = peso / (altura * altura); 
+    var tdPeso = cliente.querySelector('.peso');
+    var peso = tdPeso.textContent;
 
-tdImc.textContent = imc 
+    var tdAltura = cliente.querySelector('.altura')
+    var altura = tdAltura.textContent;
 
+    var tdImc = cliente.querySelector('.info--imc');
+
+    var pesoValido = true;
+    var alturaValido = true;
+
+    if( peso <= 0 || peso >= 500 ) { //se peso for menor que zero ou maior que 500 exiba peso inválido
+    pesoValido = false;
+    tdPeso.textContent = "Peso Inválido";
+    };
+
+    if( altura <= 0 || altura >= 3.0 ) { //se peso for menor que zero ou maior que 500 exiba peso inválido
+    alturaValido = false;
+    tdAltura.textContent = "Altura Inválida";
+    };
+
+    if (alturaValido && pesoValido){
+    var imc = peso / (altura * altura); 
+    tdImc.textContent = imc.toFixed(2); //declara o quanto que você quer de casas decimais
+    };
+}
+
+
+
+
+
+
+/*vamos fazer um loop para todos os  pacientes */
